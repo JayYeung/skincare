22f8aa9aaaee05158f80641c0aa9baf035b871cc
JayYeung <JayYeungScout@gmail.com>
2024-04-27 23:36:42 -0700

    red

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main> "hi" 
-      <p> "hi there! my name is ferit! </p>
-      <p className = "red"> this is not red </p>
-      
-
-
-      </main>
-  )  
+    return (
+        <main>
+            "hi"
+            <p> "hi there! my name is ferit! </p>
+            <p className="text-red-800"> this is not red </p>
+        </main>
+    );
 }
